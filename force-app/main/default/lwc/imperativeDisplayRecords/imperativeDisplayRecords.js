1c922974c2817e63f9de9c30fe3ba1eecf597b0e
Anh Truong <98434267+at-n-t@users.noreply.github.com>
2024-06-26 11:31:30 -0400

    Updated displayRecords.

--- force-app/main/default/lwc/imperativeDisplayRecords/imperativeDisplayRecords.js
+++ force-app/main/default/lwc/imperativeDisplayRecords/imperativeDisplayRecords.js
@@ -1,3 +1,18 @@
 import { LightningElement } from 'lwc';
+import getAccounts from '@salesforce/apex/AccountHelper.getAccounts';
 
-export default class ImperativeDisplayRecords extends LightningElement {}
+export default class ImperativeDisplayRecords extends LightningElement {
+    
+    accountResult;
+
+    handleSearch() {
+        let s = this.template.querySelector('.input').value;
+        getAccounts({ search : s})
+        .then(result => {
+            this.accountResult = result;
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }
+}
